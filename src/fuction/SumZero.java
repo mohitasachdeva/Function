7a180aebea05fc1618698504f68fdaaa1d89f045
mohita <sachdevamohita5@gmail.com>
2022-07-03 21:47:23 +0530

    show 2 dimensional array

--- src/fuction/SumZero.java
+++ src/fuction/SumZero.java
@@ -2,18 +2,18 @@ package fuction;
 
 public class SumZero {
     public static void main(String[] args) {
-        int[] arr = {3, -1, -7, -4, -5, 9, 10};
+        int[] arr  = {3, -1, -7, -4, -5, 9, 10};
 
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i< arr.length-2 ; i++) {
             int a = arr[i];
-            for (int j = i + 1; j < arr.length; j++) {
+            for (int j = i + 1; j<arr.length-1; j++) {
                 int b = arr[j];
-                for (int k = j + 1; j < arr.length; k++) {
+                for (int k = j + 1; k < arr.length; k++) {
                     int c = arr[k];
                     int sum = a + b + c;
                     if (sum == 0) {
-                        System.out.print(arr[k] + "," + arr[j] + "," + arr[k]);
+                        System.out.println(arr[i] + "," +arr[j]+  "," +arr[k]);
 
                     }
 
--- src/fuction/WindChill.java
+++ src/fuction/WindChill.java
@@ -1,8 +1,8 @@
 package fuction;
     public class WindChill {
             public static void main(String[] args) {
-                double t = Double.parseDouble(args[0]);
-                double v = Double.parseDouble(args[1]);
+                double t = 5.0;
+                double v = 3.6;
                 double w = 35.74 + 0.6215*t + (0.4275*t - 35.75) * Math.pow(v, 0.16);
                 System.out.println("Temperature = " + t);
                 System.out.println("Wind speed  = " + v);
