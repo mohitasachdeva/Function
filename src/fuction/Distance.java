17e68c6b103de58922fda87d95954f6626ff247c
mohita <sachdevamohita5@gmail.com>
2022-07-03 16:22:37 +0530

    find distance between two points

---
+++ src/fuction/Distance.java
@@ -0,0 +1,10 @@
+package fuction;
+
+public class Distance {
+    public static void main(String[] args) {
+      int x1=2 ,x2=3,y1=4,y2=6;
+      double dis;
+      dis= Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.println(" distance between two points -->" +dis);
+    }
+}
